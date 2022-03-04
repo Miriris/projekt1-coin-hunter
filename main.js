d27e6528c62905ee85689633e0e77a8bf502bf7f
Miriam <rezakmiriam@gmail.com>
2022-03-04 22:48:23 +0100

    Nastaveni pohybu podle sipek

--- main.js
+++ main.js
@@ -8,7 +8,39 @@ if (!( panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || pan
 // sem začni psát svůj program
 
 let panacek = document.querySelector('#panacek');
-let x = '100px';
-let y = '100px';
-panacek.style.left = x;
-panacek.style.top = y;
+let x = 100;
+let y = 100;
+panacek.style.left = x + 'px';
+panacek.style.top = y + 'px';
+
+document.onkeydown = function zmenaPozicePanacka(e) {
+  var event = window.event || e;
+
+  // var kod = event.keyCode;
+  //console.log(kod);
+
+  if (event.keyCode == 37) {
+    panacek.style.left =
+      parseInt(window.getComputedStyle(panacek).getPropertyValue('left')) -
+      10 +
+      'px';
+  } else if (event.keyCode == 38) {
+    panacek.style.top =
+      parseInt(window.getComputedStyle(panacek).getPropertyValue('top')) -
+      10 +
+      'px';
+  } else if (event.keyCode == 39) {
+    panacek.style.left =
+      parseInt(window.getComputedStyle(panacek).getPropertyValue('left')) +
+      10 +
+      'px';
+  } else if (event.keyCode == 40) {
+    panacek.style.top =
+      parseInt(window.getComputedStyle(panacek).getPropertyValue('top')) +
+      10 +
+      'px';
+  } else {
+    null;
+  }
+  // console.log(event);
+};
